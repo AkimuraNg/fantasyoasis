bac2b0377bc69a89b27cc15c8134740a969c3427
Duy Nguyen <duynguyen11062001@gmail.com>
2023-08-09 15:16:33 +0300

    Fix responsive

--- interface/src/components/categoryPage.js
+++ interface/src/components/categoryPage.js
@@ -5,31 +5,31 @@ import './styles/categoryPage.scss'
 
 const CategoryPage = () => {
 
-    const { category } = useParams();
-    const { data: stories, loading, error } = useFetch(`https://bedecked-elastic-whippet.glitch.me/server?category=${category}`);
-  
-    if (loading) {
-      return <div>Loading...</div>;
-    }
-  
-    if (error) {
-      return <div>Error: {error}</div>;
-    }
-  
-    return (
-      <div className='categoryPage'>
-        <h2>{category}</h2>
-        <div className='pageItems'>
-          {stories.map((story) => (
-            <div key={story.id} className='card'>
-              <h3 className='card-title'>{story.title}</h3>
-              <Link to={`/server/${story.id}`}>To Story</Link>
-              {/* <p>{story.body}</p> */}
-            </div>
-          ))}
-        </div>
+  const { category } = useParams();
+  const { data: stories, loading, error } = useFetch(`https://bedecked-elastic-whippet.glitch.me/server?category=${category}`);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div className='categoryPage'>
+      <h2>{category}</h2>
+      <div className='pageItems'>
+        {stories.map((story) => (
+          <div key={story.id} className='card'>
+            <h3 className='card-title'>{story.title}</h3>
+            <p className='card-body'>{story.author}</p>
+            <Link to={`/server/${story.id}`}>To Story</Link>
+          </div>
+        ))}
       </div>
-    );
+    </div>
+  );
 }
 
 export default CategoryPage
